5478f5a1248038a646baedc3cb04d7a2da2666f5
Jesse McConnell <jesse.mcconnell@gmail.com>
2016-12-14 07:16:31 -0600

    test layout + initial sample smoke test for issue #72 (#80)
    * test layout + initial sample smoke test
    
    * fix bad groupId
    
    * add index.html and wire up to use war file as opposed to unexploded war
    
    * removed unused classes, trimmed down dependencies and further cleaned up some framework bits

---
+++ tests/test-war-smoke/src/test/java/com/google/cloud/runtimes/jetty/tests/HelloRemoteIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.google.cloud.runtimes.jetty.tests;
+
+
+import com.google.cloud.runtime.jetty.testing.AppDeployment;
+import com.google.cloud.runtime.jetty.testing.HttpUrlUtil;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class HelloRemoteIntegrationTest extends AbstractHelloIntegrationTest {
+
+  @BeforeClass
+  public static void isServerUp() {
+    HttpUrlUtil.waitForServerUp(AppDeployment.SERVER_URI, 5, TimeUnit.MINUTES);
+  }
+
+  @Test
+  public void testGetHello() throws IOException {
+    assertTestGet(AppDeployment.SERVER_URI.resolve("/hello/"));
+  }
+
+}
