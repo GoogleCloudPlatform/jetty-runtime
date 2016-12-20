11d4f5b6f84b03221079aefb24c6e69a306b5a6b
Jesse McConnell <jesse.mcconnell@gmail.com>
2016-12-20 08:25:23 -0600

    pull AppDeployment into the base test class

--- tests/gcloud-testing-core/src/main/java/com/google/cloud/runtime/jetty/test/AbstractIntegrationTest.java
+++ tests/gcloud-testing-core/src/main/java/com/google/cloud/runtime/jetty/test/AbstractIntegrationTest.java
@@ -16,66 +16,94 @@
 
 package com.google.cloud.runtime.jetty.test;
 
-import com.google.cloud.runtime.jetty.util.AppDeployment;
 import com.google.cloud.runtime.jetty.util.HttpUrlUtil;
 
+import org.eclipse.jetty.toolchain.test.MavenTestingUtils;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.net.URI;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
 public class AbstractIntegrationTest {
 
-  private static URI localUri;
-  private static int LOCAL_TIMEOUT_SECONDS = 10; // seconds
-  private static URI remoteUri;
-  private static int REMOTE_TIMEOUT_SECONDS = 300; // seconds
+  private static int DEFAULT_LOCAL_TIMEOUT_SECONDS = 10;
+  private static int DEFAULT_REMOTE_TIMEOUT_SECONDS = 300;
+  private static int TIMEOUT_SECONDS;
+  private static final URI SERVER_URI;
 
+  static {
+
+    if ( System.getProperty("test.local") != null ) {
+      String testPort = System.getProperty("app.deploy.port");
+
+      TIMEOUT_SECONDS = DEFAULT_LOCAL_TIMEOUT_SECONDS;
+      SERVER_URI = URI.create("http://localhost:" + testPort);
+      Assert.assertNotNull("local uri required", SERVER_URI);
+
+    } else if ( System.getProperty("test.remote") != null ) {
+      String projectId = System.getProperty("app.deploy.project");
+      String version = System.getProperty("app.deploy.version");
+
+      Objects.requireNonNull(projectId, "app.deploy.project");
+      Objects.requireNonNull(version, "app.deploy.version");
+
+      String serviceId = null;
+
+      Path appYamlPath = MavenTestingUtils.getProjectFilePath("src/main/appengine/app.yaml");
+      if (Files.exists(appYamlPath)) {
+        try (BufferedReader reader = Files.newBufferedReader(appYamlPath, StandardCharsets.UTF_8)) {
+          Yaml yaml = new Yaml();
+          Map map = (Map) yaml.load(reader);
+          serviceId = (String) map.get("service");
+          if (serviceId == null) {
+            serviceId = (String) map.get("module");
+          }
+        } catch (IOException e) {
+          throw new RuntimeException("Unable to parse app.yaml", e);
+        }
+      }
+
+      StringBuilder uri = new StringBuilder();
+      uri.append("https://");
+      uri.append(version).append("-dot-");
+      if (serviceId != null) {
+        uri.append(serviceId).append("-dot-");
+      }
+      uri.append(projectId);
+      uri.append(".appspot.com/");
+
+      TIMEOUT_SECONDS = DEFAULT_REMOTE_TIMEOUT_SECONDS;
+      SERVER_URI = URI.create(uri.toString());
+    } else {
+      throw new IllegalArgumentException("test.local or test.remote property required");
+    }
+  }
 
   /**
-   * Get the URI to test with
-   * @return
+   * Get the URI to test with.
+   * @return the server URI
    */
   public URI getUri() {
-    if (localUri != null) {
-      return localUri;
-    }
-    else {
-      return remoteUri;
-    }
+    return SERVER_URI;
   }
 
   /**
    * Waits for the server to start which needs to be completed for unit
    * tests to run.
    *
-   * @throws Exception
+   * @throws Exception catch-all for issues checking server startup
    */
   @BeforeClass
   public static void waitForServerUp() throws Exception {
-
-    // Local test property
-    String testPort = System.getProperty("app.deploy.port");
-
-    // Remote test property
-    String projectId = System.getProperty("app.deploy.project");
-
-    // Local wait for
-    if ( testPort != null ) {
-
-      localUri = new URI("http://localhost:" + testPort);
-      Assert.assertNotNull("local uri required", localUri);
-
-      HttpUrlUtil.waitForServerUp(localUri, LOCAL_TIMEOUT_SECONDS, TimeUnit.SECONDS);
-    }
-
-    // Remote wait for
-    if (projectId != null ) {
-      remoteUri = AppDeployment.SERVER_URI;
-      Assert.assertNotNull("remote uri required", remoteUri);
-
-      HttpUrlUtil.waitForServerUp(remoteUri, REMOTE_TIMEOUT_SECONDS, TimeUnit.SECONDS);
-    }
+    HttpUrlUtil.waitForServerUp(SERVER_URI, TIMEOUT_SECONDS, TimeUnit.SECONDS);
   }
 }
