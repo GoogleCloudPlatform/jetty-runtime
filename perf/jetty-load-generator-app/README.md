3efa5fdaf1d14a5aafec446423cfeeb8b8cc4c37
olivier lamy <olamy@apache.org>
2017-01-28 09:53:46 +1100

    fix build using only jetty-home
    Signed-off-by: olivier lamy <olamy@apache.org>

--- perf/jetty-load-generator-app/README.md
+++ perf/jetty-load-generator-app/README.md
@@ -6,19 +6,9 @@ Run the image using `docker run -it -p 8080:8080  webtide/jetty-load-generator-a
 
 Build and deploy to gcloud: `mvn clean deploy -pl :jetty-load-generator-app-gcloud -am -Pgcloud`
 
-# jetty-load-generator-app (WIP)
+Build: `mvn clean package -pl :jetty-load-generator-app-embedded -am -DskipTests -Djetty9.version=9.4.0.v20161208`
 
-Embeded app for Jetty Load Testing in branch embedded-app
-
-Build: `mvn clean package -pl :jetty-load-generator-app-embedded -am -DskipTests -Djetty.version=9.3.13.M0`
-
--Djetty.version= the version version you want to include in the uber jar
-
-Start: `java -jar target/app-0.0.1-SNAPSHOT.jar --port=9090`
-
-When using random port 0, the local port is write in a file called jetty.local.port
-
-File upload: `curl -X PUT -d @pom.xml http://localhost:9090/upload`
+Note: it works only for 9.4+ versions
 
 
 
--- perf/jetty-load-generator-app/jetty-load-generator-app-webapp/src/main/java/com/webtide/jetty/load/generator/web/UploadServlet.java
+++ perf/jetty-load-generator-app/jetty-load-generator-app-webapp/src/main/java/com/webtide/jetty/load/generator/web/UploadServlet.java
@@ -1,20 +1,18 @@
-//
-//  ========================================================================
-//  Copyright (c) 1995-2016 Mort Bay Consulting Pty. Ltd.
-//  ------------------------------------------------------------------------
-//  All rights reserved. This program and the accompanying materials
-//  are made available under the terms of the Eclipse Public License v1.0
-//  and Apache License v2.0 which accompanies this distribution.
-//
-//      The Eclipse Public License is available at
-//      http://www.eclipse.org/legal/epl-v10.html
-//
-//      The Apache License v2.0 is available at
-//      http://www.opensource.org/licenses/apache2.0.php
-//
-//  You may elect to redistribute this code under either of these licenses.
-//  ========================================================================
-//
+/*
+ * Copyright (C) 2016 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.webtide.jetty.load.generator.web;
 
@@ -22,41 +20,34 @@ import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+
 
 /**
- *
+ * Simple servlet upload.
  */
 @WebServlet( "/upload" )
-public class UploadServlet
-    extends HttpServlet
-{
+public class UploadServlet extends HttpServlet {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger( UploadServlet.class );
+  private static final Logger LOGGER = LoggerFactory.getLogger( UploadServlet.class );
 
-    @Override
-    protected void doGet( HttpServletRequest req, HttpServletResponse resp )
-        throws ServletException, IOException
-    {
-        String answer = "GET does nothing";
-        LOGGER.info( answer );
-        resp.getWriter().print( answer );
-    }
+  @Override
+  protected void doGet( HttpServletRequest req, HttpServletResponse resp )
+    throws ServletException, IOException {
+    String answer = "GET does nothing";
+    LOGGER.info( answer );
+    resp.getWriter().print( answer );
+  }
 
-    @Override
-    protected void doPut( HttpServletRequest req, HttpServletResponse resp )
-        throws ServletException, IOException
-    {
-        //Path tmp = Files.createTempFile( "loadgenerator", ".jetty" );
-        //try (OutputStream outputStream = Files.newOutputStream( tmp ))
-        //{
-        IOUtils.copy( req.getInputStream(), resp.getOutputStream() );
-        //}
-
-    }
+  @Override
+  protected void doPut( HttpServletRequest req, HttpServletResponse resp )
+    throws ServletException, IOException {
+    IOUtils.copy( req.getInputStream(), resp.getOutputStream() );
+  }
 }
