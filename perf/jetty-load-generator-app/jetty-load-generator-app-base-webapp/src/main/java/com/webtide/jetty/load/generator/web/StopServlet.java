463a40bfc52627c3790a58bf091a708b02b4c9d4
olivier lamy <olamy@apache.org>
2017-01-27 20:42:25 +1100

    simplify as we do not use embedded here
    Signed-off-by: olivier lamy <olamy@apache.org>

---
+++ perf/jetty-load-generator-app/jetty-load-generator-app-base-webapp/src/main/java/com/webtide/jetty/load/generator/web/StopServlet.java
@@ -0,0 +1,47 @@
+//
+//  ========================================================================
+//  Copyright (c) 1995-2016 Mort Bay Consulting Pty. Ltd.
+//  ------------------------------------------------------------------------
+//  All rights reserved. This program and the accompanying materials
+//  are made available under the terms of the Eclipse Public License v1.0
+//  and Apache License v2.0 which accompanies this distribution.
+//
+//      The Eclipse Public License is available at
+//      http://www.eclipse.org/legal/epl-v10.html
+//
+//      The Apache License v2.0 is available at
+//      http://www.opensource.org/licenses/apache2.0.php
+//
+//  You may elect to redistribute this code under either of these licenses.
+//  ========================================================================
+//
+package com.webtide.jetty.load.generator.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ *
+ */
+public class StopServlet
+    extends HttpServlet
+{
+
+    private static final Logger LOGGER = LoggerFactory.getLogger( StopServlet.class );
+
+    @Override
+    protected void doGet( HttpServletRequest req, HttpServletResponse resp )
+        throws ServletException, IOException
+    {
+
+        LOGGER.info( "stop application" );
+
+        System.exit( 0 );
+    }
+}
