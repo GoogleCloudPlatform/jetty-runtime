82bc2022e9078711f35e01084e6087b4a906e4ba
Alex Sloan <alexsloan@google.com>
2017-02-27 15:20:34 -0500

    Add cloudbuild script and configuration

---
+++ scripts/cloudbuild.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Copyright 2016 Google Inc. All rights reserved.
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+projectRoot=`dirname $0`/..
+buildProperties=$projectRoot/target/classes/build.properties
+
+# reads a property value from a .properties file
+function read_prop {
+  grep "${1}" $buildProperties | cut -d'=' -f2
+}
+
+# invoke local maven to generate build properties file
+mvn generate-resources
+
+DOCKER_NAMESPACE='gcr.io/$PROJECT_ID'
+RUNTIME_NAME='jetty'
+DOCKER_TAG_LONG=$(read_prop "docker.tag.long")
+
+export IMAGE="${DOCKER_NAMESPACE}/${RUNTIME_NAME}:${DOCKER_TAG_LONG}"
+echo "IMAGE: $IMAGE"
+
+mkdir -p $projectRoot/target
+envsubst < $projectRoot/cloudbuild.yaml.in > $projectRoot/target/cloudbuild.yaml
+
+gcloud container builds submit --config=$projectRoot/target/cloudbuild.yaml .
+
