f32f853d016103d202bef730dd9ecc7bfd0375e5
olivier lamy <olamy@webtide.com>
2017-04-13 13:52:46 +1000

    fix hostname null
    Signed-off-by: olivier lamy <olamy@webtide.com>

--- perf/perf-runner/src/main/java/com/google/cloud/runtime/jetty/perf/PerfRunner.java
+++ perf/perf-runner/src/main/java/com/google/cloud/runtime/jetty/perf/PerfRunner.java
@@ -29,7 +29,7 @@ import org.mortbay.jetty.load.generator.listeners.CollectorInformations;
 import org.mortbay.jetty.load.generator.listeners.QpsListenerDisplay;
 import org.mortbay.jetty.load.generator.listeners.RequestQueuedListenerDisplay;
 import org.mortbay.jetty.load.generator.listeners.Utils;
-import org.mortbay.jetty.load.generator.listeners.latency.GlobalLatencyTimeDisplayListener;
+import org.mortbay.jetty.load.generator.listeners.latency.LatencyTimeDisplayListener;
 import org.mortbay.jetty.load.generator.listeners.report.GlobalSummaryListener;
 import org.mortbay.jetty.load.generator.starter.LoadGeneratorStarter;
 import org.mortbay.jetty.load.generator.starter.LoadGeneratorStarterArgs;
@@ -67,6 +67,13 @@ public class PerfRunner {
     ensureNetwork(runnerArgs,20);
 
     LoadGeneratorRunner runner = new LoadGeneratorRunner( runnerArgs );
+    String hostname = "";
+    try {
+      hostname = InetAddress.getLocalHost().getHostName();
+    } catch ( Exception e ) {
+      LOGGER.info( "ignore cannot get hostname:" + e.getMessage() );
+    }
+    runner.host = hostname;
     LOGGER.info( "start load test" );
     runner.run();
     LOGGER.info( "load test done" );
@@ -76,14 +83,6 @@ public class PerfRunner {
     long totalRequestCommitted = latencyTimeSummary.getTotalCount();
     long start = latencyTimeSummary.getStartTimeStamp();
     long end = latencyTimeSummary.getEndTimeStamp();
-
-    String hostname = "";
-    try {
-      hostname = InetAddress.getLocalHost().getHostName();
-    } catch ( Exception e ) {
-      LOGGER.info( "ignore cannot get hostname:" + e.getMessage() );
-    }
-    runner.host = hostname;
 
     LOGGER.info( "" );
     LOGGER.info( "" );
@@ -100,11 +99,16 @@ public class PerfRunner {
     long qps = totalRequestCommitted / timeInSeconds;
     LOGGER.info( "host '" + hostname + "' estimated QPS : " + qps );
     LOGGER.info( "----------------------------------------------------");
-    LOGGER.info( "perfmetric:max_latency:" + latencyTimeSummary.getMaxValue());
-    LOGGER.info( "perfmetric:min_latency:" + latencyTimeSummary.getMinValue());
-    LOGGER.info( "perfmetric:ave_latency:" + latencyTimeSummary.getMean());
-    LOGGER.info( "perfmetric:50_latency:" + latencyTimeSummary.getValue50());
-    LOGGER.info( "perfmetric:90_latency:" + latencyTimeSummary.getValue90());
+    LOGGER.info( "perfmetric:max_latency:"
+                     + fromNanostoMillis(latencyTimeSummary.getMaxValue()));
+    LOGGER.info( "perfmetric:min_latency:"
+                     + fromNanostoMillis(latencyTimeSummary.getMinValue()));
+    LOGGER.info( "perfmetric:ave_latency:"
+                     + fromNanostoMillis(Math.round(latencyTimeSummary.getMean())));
+    LOGGER.info( "perfmetric:50_latency:"
+                     + fromNanostoMillis(latencyTimeSummary.getValue50()));
+    LOGGER.info( "perfmetric:90_latency:"
+                     + fromNanostoMillis(latencyTimeSummary.getValue90()));
     LOGGER.info( "----------------------------------------------------");
     LOGGER.info( "" );
 
@@ -122,6 +126,11 @@ public class PerfRunner {
 
   }
 
+
+  private static long fromNanostoMillis(long nanosValue) {
+    return TimeUnit.MILLISECONDS.convert( nanosValue, TimeUnit.NANOSECONDS );
+  }
+
   private static class LoadGeneratorRunner extends LoadGeneratorStarter {
 
     private String host;
@@ -135,20 +144,25 @@ public class PerfRunner {
         // FIXME those values need to be configurable!! //
         new RequestQueuedListenerDisplay(10, 10, TimeUnit.SECONDS);
 
-    private GlobalLatencyTimeDisplayListener globalLatencyTimeDisplayListener =
-        new GlobalLatencyTimeDisplayListener( 10, 10, TimeUnit.SECONDS );
+    private LatencyTimeDisplayListener latencyTimeDisplayListener =
+        new LatencyTimeDisplayListener( 10, 10, TimeUnit.SECONDS );
 
     public LoadGeneratorRunner( LoadGeneratorStarterArgs runnerArgs ) {
       super( runnerArgs );
-      this.globalLatencyTimeDisplayListener.addValueListener( histogram -> {
+      this.latencyTimeDisplayListener.addValueListener( histogram -> {
         LOGGER.info( "host '" + host );
         LOGGER.info( "----------------------------------------------------");
         LOGGER.info( "perfmetric_run:total:" + histogram.getTotalCount());
-        LOGGER.info( "perfmetric_run:max_latency:" + histogram.getMaxValue());
-        LOGGER.info( "perfmetric_run:min_latency:" + histogram.getMinValue());
-        LOGGER.info( "perfmetric_run:ave_latency:" + histogram.getMean());
-        LOGGER.info( "perfmetric_run:50_latency:" + histogram.getValueAtPercentile( 50 ));
-        LOGGER.info( "perfmetric_run:90_latency:" + histogram.getValueAtPercentile( 90 ));
+        LOGGER.info( "perfmetric_run:max_latency:"
+                         + fromNanostoMillis( histogram.getMaxValue()) );
+        LOGGER.info( "perfmetric_run:min_latency:"
+                         + fromNanostoMillis( histogram.getMinValue()) );
+        LOGGER.info( "perfmetric_run:ave_latency:"
+                         + fromNanostoMillis( Math.round( histogram.getMean())) );
+        LOGGER.info( "perfmetric_run:50_latency:"
+                         + fromNanostoMillis( histogram.getValueAtPercentile( 50 )));
+        LOGGER.info( "perfmetric_run:90_latency:"
+                         + fromNanostoMillis( histogram.getValueAtPercentile( 90 )));
         LOGGER.info( "----------------------------------------------------");
       } );
     }
@@ -165,7 +179,7 @@ public class PerfRunner {
 
     @Override
     protected Resource.Listener[] getResourceListeners() {
-      return new Resource.Listener[]{globalSummaryListener};
+      return new Resource.Listener[]{globalSummaryListener, latencyTimeDisplayListener };
     }
 
     public CollectorInformations getLatencyTimeSummary() {
