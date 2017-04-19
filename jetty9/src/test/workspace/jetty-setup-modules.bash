a7e7f3731d9b349adfb770e66cd418b4d993a8ed
Greg Wilkins <gregw@webtide.com>
2017-04-19 15:22:53 +1000

    Simplify entrypoint args processing
    The $@ arg array is kept complete between all scripts.

--- jetty9/src/test/workspace/jetty-setup-modules.bash
+++ jetty9/src/test/workspace/jetty-setup-modules.bash
@@ -1,6 +1,5 @@
 #!/bin/bash
 set - java start.jar arg
-JAVA_OPTS="-X"
 JETTY_PROPERTIES="prop0=value0,prop1=value1"
 JETTY_MODULES_ENABLE="mod0,mod1"
 JETTY_MODILES_DISABLE="gone0,gone1"
@@ -9,10 +8,10 @@ touch $JETTY_BASE/start.d/gone0.ini
 touch $JETTY_BASE/start.d/gone1.ini
 
 source /setup-env.d/50-jetty.bash
-if [ "$(echo ${JETTY_ARGS} | xargs)" != "start.jar arg prop0=value0 prop1=value1 --module=mod0 --module=mod1" ]; then 
-  echo "JETTY_ARGS='${JETTY_ARGS}'"
-elif [ "$(echo ${JAVA_OPTS} | xargs)" != "-X start.jar arg prop0=value0 prop1=value1 --module=mod0 --module=mod1" ]; then
-  echo "JAVA_OPTS='${JAVA_OPTS}'"
+if [ "$(echo ${JETTY_ARGS} | xargs)" != "prop0=value0 prop1=value1 --module=mod0 --module=mod1" ]; then 
+  echo "JETTY_ARGS='$(echo ${JETTY_ARGS} | xargs)'"
+elif [ "$(echo $@ | xargs)" != "java start.jar arg prop0=value0 prop1=value1 --module=mod0 --module=mod1" ]; then
+  echo "@='$(echo $@ | xargs)'"
 elif [ -x JETTY_BASE/start.d/gone0.ini ]; then
   echo gone0.ini not deleted
 elif [ -x JETTY_BASE/start.d/gone1.ini ]; then
