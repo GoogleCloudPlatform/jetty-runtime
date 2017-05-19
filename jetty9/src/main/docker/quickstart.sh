3ced5f63ad8ef88341ba478620f8923d20cb1a3c
Damien Cassan <damiencassan@google.com>
2017-05-19 11:35:07 -0400

    Refactor the quickstart script to expose the different steps

--- jetty9/src/main/docker/quickstart.sh
+++ jetty9/src/main/docker/quickstart.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # Enable quickstart module for Jetty and eagerly generate the quickstart-web.xml
 
-source /docker-entrypoint.bash bash
-
 # Generate configuration files for quickstart
 if [ ! -e "$JETTY_BASE/webapps/root/WEB-INF/quickstart-web.xml" ]; then
   pushd ${JETTY_BASE}
   java -jar ${JETTY_HOME}/start.jar --add-to-start=quickstart
-  QUICKSTART_CLASSPATH=$(java -jar ${JETTY_HOME}/start.jar --dry-run)
-  # Replace the Xml configuration class with the Quickstart pre configuration class
-  $( echo "$QUICKSTART_CLASSPATH" | sed 's/org.eclipse.jetty.xml.XmlConfiguration.*/org.eclipse.jetty.quickstart.PreconfigureQuickStartWar webapps\/root.war/')
+  JETTY_START_COMMAND=$(java -jar ${JETTY_HOME}/start.jar --dry-run)
+   # Replace the Xml configuration class with the Quickstart pre configuration class
+  QUICKSTART_START_COMMAND=$(echo ${JETTY_START_COMMAND} | sed 's/org.eclipse.jetty.xml.XmlConfiguration.*/org.eclipse.jetty.quickstart.PreconfigureQuickStartWar/')
+  /docker-entrypoint.bash ${QUICKSTART_START_COMMAND} "$ROOT_WAR"
   popd
 fi
