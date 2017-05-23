f7f2c3d8f9b7ee0da1a0c5e881a337baae9a49f6
João Martins <joaoandremartins@users.noreply.github.com>
2017-05-23 17:14:29 -0400

    Exports CDBG_APP_WEB_INF_DIR before 20-debug-env.bash runs. (#175)
    * Exports CDBG_APP_WEB_INF_DIR before 20-debug-env.bash runs.
    
    * Missing new line.
    
    * Renaming
    
    * Add structure test for debugger jvm args (#177)
    
    * Add structure test for debugger jvm args
    
    * Use jetty base
    
    * Add quotes

---
+++ jetty9/src/test/workspace/jetty-setup-debugger.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+EXPECTED_DBG_AGENT="-agentpath:/opt/cdbg/cdbg_java_agent.so=--log_dir=/var/log/app_engine,--alsologtostderr=true,--cdbg_extra_class_path=${JETTY_BASE}/webapps/root/WEB-INF/classes:${JETTY_BASE}/webapps/root/WEB-INF/lib"
+ACTUAL_DBG_AGENT="$(export GAE_INSTANCE=instance; /docker-entrypoint.bash env | grep DBG_AGENT | cut -d '=' -f 1 --complement)"
+
+if [ "$ACTUAL_DBG_AGENT" != "$EXPECTED_DBG_AGENT" ]; then
+  echo "DBG_AGENT='$(echo ${ACTUAL_DBG_AGENT})'"
+  exit 1
+else
+  echo OK
+fi
