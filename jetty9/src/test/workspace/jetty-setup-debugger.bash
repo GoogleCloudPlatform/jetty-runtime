bf311f6ee625d108b36007f3b53c78660e5b762c
Alex Sloan <alexsloan@google.com>
2017-05-23 10:52:41 -0400

    Use jetty base

--- jetty9/src/test/workspace/jetty-setup-debugger.bash
+++ jetty9/src/test/workspace/jetty-setup-debugger.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-EXPECTED_DBG_AGENT='-agentpath:/opt/cdbg/cdbg_java_agent.so=--log_dir=/var/log/app_engine,--alsologtostderr=true,--cdbg_extra_class_path=/var/lib/jetty/webapps/root/WEB-INF/classes:/var/lib/jetty/webapps/root/WEB-INF/lib'
+EXPECTED_DBG_AGENT="-agentpath:/opt/cdbg/cdbg_java_agent.so=--log_dir=/var/log/app_engine,--alsologtostderr=true,--cdbg_extra_class_path=${JETTY_BASE}/webapps/root/WEB-INF/classes:${JETTY_BASE}/webapps/root/WEB-INF/lib"
 ACTUAL_DBG_AGENT="$(export GAE_INSTANCE=instance; /docker-entrypoint.bash env | grep DBG_AGENT | cut -d '=' -f 1 --complement)"
 
 if [ $ACTUAL_DBG_AGENT != "$EXPECTED_DBG_AGENT" ]; then
