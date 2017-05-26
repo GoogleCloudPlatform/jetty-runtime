9fb48969cd88fea880cfc887dc8909dad782a0c4
Alex Sloan <alexsloan@google.com>
2017-05-26 16:51:02 -0400

    Store start command in env variable instead of file

--- jetty9/src/main/docker/gen_start_command.sh
+++ jetty9/src/main/docker/gen_start_command.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-START_COMMAND_OUT=$1
-
-java -jar -Djetty.base=${JETTY_BASE} ${JETTY_HOME}/start.jar $JETTY_ARGS --dry-run --exec-properties="${START_COMMAND_OUT}.properties" &> $START_COMMAND_OUT
+java -jar -Djetty.base=${JETTY_BASE} ${JETTY_HOME}/start.jar $JETTY_ARGS --dry-run
--- jetty9/src/main/docker/jetty_start.sh
+++ jetty9/src/main/docker/jetty_start.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 DIR=$(dirname $0)
-JETTY_START_COMMAND="/scripts/jetty/jetty-start-command"
 
 if [ ! -f $JETTY_START_COMMAND ]; then
-  source $DIR/gen_start_command.sh $JETTY_START_COMMAND
+  export JETTY_START_COMMAND=$(source $DIR/gen_start_command.sh)
 fi
+
+echo $JETTY_START_COMMAND
 
 set -- $(cat $JETTY_START_COMMAND)
 source /docker-entrypoint.bash
