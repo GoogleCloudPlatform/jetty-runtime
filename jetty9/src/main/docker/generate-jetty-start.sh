814835dd3d2c3c16de9151939df5d0ef2bb64400
Alex Sloan <alexsloan@google.com>
2017-05-31 13:54:17 -0400

    Fix entrypoint name in generate-jetty-start

--- jetty9/src/main/docker/generate-jetty-start.sh
+++ jetty9/src/main/docker/generate-jetty-start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rm -f /jetty-start /jetty-start.properties
-/docker-entrypoint.sh --dry-run --exec-properties=/jetty-start.properties > /jetty-start
+/docker-entrypoint.bash --dry-run --exec-properties=/jetty-start.properties > /jetty-start
