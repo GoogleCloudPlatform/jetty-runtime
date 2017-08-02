f35c710dca3bec3ba83269e41ebe7873ee727f6a
Alex Sloan <alexsloan@google.com>
2017-08-02 15:01:39 -0400

    Specify gke zone, run scripts in strict mode (#204)

--- build/scripts/gke/test.sh
+++ build/scripts/gke/test.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+set -e
+
 DEPLOYED_APP_URL=$(cat target/gke-app-ip.txt)
 
 /testsuite/driver.py --url=${DEPLOYED_APP_URL} $@
