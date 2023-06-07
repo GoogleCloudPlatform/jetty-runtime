68683f9ed4d09f8eca6d21abaae47cab7c251bfa
shreejad <59456116+shreejad@users.noreply.github.com>
2023-06-07 18:49:07 +0000

    Remove Cloud Debugger references.
    Cloud Debugger has reached end-of-support on May 31, 2023. The main removal of debugger integration has been done in the OpenJDK codebase, this change is a cleanup of extra steps being done in Jetty.

--- jetty9/src/main/docker/15-debug-env-jetty.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Adds bits of configuration necessary for the Cloud Debugger to work with the Jetty image.
-# It needs to run before 20-debug-env.bash from the openjdk image.
-
-# Set CDBG_APP_WEB_INF_DIR, used by CDBG in format-env-appengine-vm.sh
-export CDBG_APP_WEB_INF_DIR="${JETTY_BASE}/webapps/root/WEB-INF"
